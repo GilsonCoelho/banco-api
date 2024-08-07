70f5c9d5f283c418af5655e548da5032cc677090
GilsonCoelho <coelho.gilson@gmail.com>
2024-08-07 15:57:05 -0300

    removendo interfaces sem uso

--- src/main/java/br/com/gsc/banco_api/domain/repository/CardRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.gsc.banco_api.domain.repository;
-
-import br.com.gsc.banco_api.domain.model.Card;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CardRepository  extends JpaRepository<Card, Long> {
-}
--- src/main/java/br/com/gsc/banco_api/domain/repository/FeatureRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.gsc.banco_api.domain.repository;
-
-import br.com.gsc.banco_api.domain.model.Feature;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FeatureRepository extends JpaRepository<Feature, Long> {
-}
